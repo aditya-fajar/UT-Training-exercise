df37bb7e25039a4a67c5f3a7fba114140f650c11
adit <adityafajar1806@gmail.com>
2019-12-16 14:38:56 +0700

    Exercise 3

---
+++ P1-3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Ganti ^ menjadi main jika mau mencoba
+
+	a := []string{"foo1", "Bar1", "foo2", "Bar2"}
+
+	for _, b := range a {
+		fmt.Println(b)
+	}
+}
